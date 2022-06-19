717dd66f52ab6e32454817643fd182a0c138de59
Your Name <you@example.com>
2022-06-19 15:36:14 +0530

    [Megha] Add: Database Created

---
+++ EmployeePayrollADO/Employee.cs
@@ -0,0 +1,47 @@
+﻿using DocumentFormat.OpenXml.Drawing.Diagrams;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollADO
+{
+    public class Employee
+    {
+
+        public static string connection = "Data Source=(localdb)\\MSSQLLocaldb;Initial Catalog=Pay_Services";
+        SqlConnection sqlConnection = new SqlConnection(connection);
+
+        public void SetConnection()
+        {
+            if (sqlConnection != null && sqlConnection.State.Equals(ConnectionState.Closed))
+            {
+                try
+                {
+                    sqlConnection.Open();
+                }
+                catch (Exception)
+                {
+                    throw new CustomException(CustomException.ExceptionType.Connection_Failed, "Connection Failed");
+                }
+            }
+        }
+        public void Close()
+        {
+            if (sqlConnection != null && !sqlConnection.State.Equals(ConnectionState.Open))
+            {
+                try
+                {
+                    sqlConnection.Close();
+                }
+                catch (Exception)
+                {
+                    throw new CustomException(CustomException.ExceptionType.Connection_Failed, "Connection Failed");
+                }
+            }
+        }
+    }
+}
--- EmployeePayrollADO/Program.cs
+++ EmployeePayrollADO/Program.cs
@@ -1,42 +1,13 @@
 ﻿using System;
-using EmployeePayrollADO;
-public class Program
+namespace EmployeePayrollADO
 {
-    public static void Main(String[] args)
+    public class Program
     {
-        Console.WriteLine("\t\t\t\t\tWELCOME TO EMPLOYEE PAROLL SERVICE PROGRAM !\t\t\t\t\t\n");
-        EmployeeRepo payrollService = new EmployeeRepo();
-        bool verify = true;
-
-        while (verify)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("\nEnter\n1. To insert data into database\n2.Exit\n");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
-            {
-                case 1:
-                    EmployeeModel addMdl = new EmployeeModel();
-                    addMdl.Name = "Megha";
-                    addMdl.Salary = 2000;
-                    addMdl.StartDate = Convert.ToDateTime("07-02-2017");
-                    addMdl.Gender = "F";
-                    addMdl.PhoneNumber = 9123456;
-                    addMdl.Address = "Bangalore";;
-                    addMdl.Department = "Sales";
-                    addMdl.BasicPay = 10000;
-                    addMdl.Deduction = 500;
-                    addMdl.Taxablepayn = 500;
-                    addMdl.IncomePay = 500;
-                    addMdl.NetPay = 2000;
-                    payrollService.AddEmployee(addMdl);
-                    break;
-                case 2:
-                    verify = false;
-                    break;
-                default:
-                    Console.WriteLine("Enter valid option !\n");
-                    break;
-            }
+            Employee employeeData = new Employee();
+            employeeData.SetConnection();
+            employeeData.Close();
         }
     }
 }
