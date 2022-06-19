739e8e644d42fe38e58df3ce65b7d4d7ba22faaf
Your Name <you@example.com>
2022-06-19 16:46:35 +0530

    [Megha] Add: Stored Procedure is Updated

--- EmployeePayrollADO/CustomException.cs
+++ EmployeePayrollADO/CustomException.cs
@@ -4,30 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-<<<<<<< HEAD
+
 
 namespace EmployeePayrollADO
 
-{ 
-=======
-namespace EmployeePayrollADO
 {
->>>>>>> UC2-RetrieveFromDatabase
-    public class CustomException : Exception
-    {
-        public enum ExceptionType
-        {
-            No_data_found, Connection_Failed
-        }
-        public ExceptionType exceptionType;
-        public CustomException(ExceptionType exceptionType, string message) : base(message)
-        {
-            this.exceptionType = exceptionType;
-        }
 
+    namespace EmployeePayrollADO
+    {
+
+        public class CustomException : Exception
+        {
+            public enum ExceptionType
+            {
+                No_data_found, Connection_Failed
+            }
+            public ExceptionType exceptionType;
+            public CustomException(ExceptionType exceptionType, string message) : base(message)
+            {
+                this.exceptionType = exceptionType;
+            }
+
+        }
     }
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> UC2-RetrieveFromDatabase
--- EmployeePayrollADO/Program.cs
+++ EmployeePayrollADO/Program.cs
@@ -12,4 +12,5 @@ namespace EmployeePayrollADO
             employeeData.UpdateSalary();
         }
     }
+    
 }
